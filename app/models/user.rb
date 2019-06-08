f914fa0e82435b59d23f3239600b18af8db4de82
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-06-08 21:55:30 +0100

    Added a difference between a renter and admin and created user seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
+  has_many :bookings, class_name: "Booking", foreign_key: "admin_id", dependent: :destroy
+  has_many :bookings, class_name: "Booking", foreign_key: "renter_id", dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  {
+    email: "jo@ongert.com",
+    password: "123456",
+    first_name: "Jo",
+    last_name: "Ongert",
+    phone: "+49987 763 362",
+    address: "1 Black Forest Road",
+    admin: true
+  },
+  {
+    email: "dave@gmail.com",
+    password: "123456",
+    first_name: "Dave",
+    last_name: "Smith",
+    phone: "+447926 825 183",
+    address: "28 Avenue Road",
+    admin: false
+  },
+  {
+    email: "sergio@hotmail.com",
+    password: "123456",
+    first_name: "Sergio",
+    last_name: "Garcia",
+    phone: "+32 12739 2172",
+    address: "99 Hola Lane",
+    admin: false
+  }
+]
+
+puts "Creating users..."
+User.create!(users)
+puts "One admin and two renters created!"
